0224bb28a9f7d941b71e9cf923a8638b60bf2045
Jonathan Lehrer <jonathan.lehrer@gmail.com>
2014-04-25 16:25:45 -0700

    added code to sanity check the results. cleaned up variable names and comments

--- GCD Programming Assignment.R
+++ GCD Programming Assignment.R
@@ -24,11 +24,11 @@ test <- cbind(x_test,y_test,subject_test)
 dat <- rbind(train,test)
 
 #read in and apply the variable names
-features <- read.table("features.txt",header=F)
+features <- read.table("./UCI HAR Dataset/features.txt",header=F)
 colnames(dat) <- c(as.character(features[,2]),"activity","subject")
 
 #read in and apply the activity names (convert codes into descriptive labels)
-activity <- read.table("activity_labels.txt",header=F)
+activity <- read.table("./UCI HAR Dataset/activity_labels.txt",header=F)
 dat$activity <- factor(dat$activity,levels=activity$V1, labels=activity$V2)
 
 #subset the data for only mean and std
@@ -36,9 +36,29 @@ meancol <- grep("mean()",colnames(dat),fixed=T,value=T)
 stdcol <- grep("std()",colnames(dat),fixed=T,value=T)
 dat_trim <- dat[,c("activity","subject",meancol,stdcol)]
 
-install.packages("reshape2")
+
+#summarize
+#install.packages("reshape2") #only necessary the first time
 library(reshape2)
 
+#melt the dataset then recast to take the average of all variables by subject and activity
 dat_melt <- melt(dat_trim,id.vars=c("subject","activity"))
 dat_cast <- dcast(dat_melt,subject+activity ~ variable,mean)
 write.table(dcast,'tidy.txt')
+
+
+
+
+#check by using a different approach
+#split apply combine
+x <- split(dat_trim,list(dat_trim$subject,dat_trim$activity))
+x2 <- lapply(x,subset,select=3:68)
+x3 <- sapply(x2,colMeans)
+
+#transpose and reorder the two results to align the data
+x4 <- t(x3)
+dc4 <- as.matrix(dat_cast[order(dat_cast$activity,dat_cast$subject),3:68])
+
+#verify that the data is equal
+all.equal(x4,dc4, check.attributes=F)
+
